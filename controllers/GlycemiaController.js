82b94b34f2499d21a850c95d1c6c3f7d05f15813
LaraGraziela <62261777+LaraGraziela@users.noreply.github.com>
2022-12-03 16:27:43 -0300

    create new routes

--- controllers/GlycemiaController.js
+++ controllers/GlycemiaController.js
@@ -1,10 +1,55 @@
-
-const database = require('../models');
+const database = require("../models");
 
 class GlycemiaController {
-    static async test(req, res){
-        res.send({message: "Hello World"});
+  static async test(req, res) {
+    res.send({ message: "Hello World" });
+  }
+
+  static async getAll(req, res) {
+    try {
+      const glycemia = await database.glycemia.findAll();
+      return res.status(200).json(glycemia);
+    } catch (error) {
+      return res.status(500).json(error.message);
     }
+  }
+  static async getOne(req, res) {
+    try {
+      const { id } = req.params;
+      const glycemia = await database.glycemia.findOne({ where: { id: Number(id) } });
+      console.log(glycemia);
+      return res.status(200).json(glycemia);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async update(req, res) {
+    try {
+      const { id } = req.params;
+      const glycemia = await database.glycemia.update(req.body, { where: { id: Number(id) } });
+      return res.status(200).json(glycemia);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const glycemia = await database.glycemia.destroy({ where: { id: Number(id) } });
+      return res.status(200).json(glycemia);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async create (req, res) {
+    try {
+      const glycemia = await database.glycemia.create(req.body);
+      return res.status(200).json(glycemia);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = GlycemiaController;
--- routes/GlycemiaRoutes.js
+++ routes/GlycemiaRoutes.js
@@ -4,5 +4,10 @@ const GlycemiaController = require("../controllers/GlycemiaController");
 const router = Router();
 
 router.get("/test", GlycemiaController.test);
+router.get("/glycemia", GlycemiaController.getAll);
+router.get("/glycemia/:id", GlycemiaController.getOne);
+router.put("/glycemia/:id", GlycemiaController.update);
+router.delete("/glycemia/:id", GlycemiaController.delete);
+router.post("/glycemia", GlycemiaController.create);
 
 module.exports = router;
